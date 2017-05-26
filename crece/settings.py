37c3ce3a64fb3b50bb18e5b6bdd89fc659807012
crisdapu21@gmail.com <crisdapu21@gmail.com>
2017-05-26 03:06:51 -0500

    Fix Imagen Certificado

--- crece/settings.py
+++ crece/settings.py
@@ -48,15 +48,6 @@ TEMPLATES = [
 from django.contrib.messages import constants as message_constants
 MESSAGE_LEVEL = message_constants.DEBUG
 
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-"""
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -67,7 +58,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
